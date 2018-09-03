8cb855ed9bd1803de1c3f976fac0fb83850d86b7
Julia Grötsch <julia@groetsch.org>
2018-09-03 17:43:11 +0200

    deleted unused code

--- Code/Usability Testing Platform/js/video.js
+++ Code/Usability Testing Platform/js/video.js
@@ -3,26 +3,13 @@ App.video = function () {
 
     "use strict";
     var that = {},
-    playerContainer,
-    videoUrl = "rtmp://104.248.33.174/live/test",
-    videoUrl1 = "https://www.youtube.com/embed/watch?v=dDxmVe3YCK0&index=1&list=PLN6V-PnVANX-mG1M7khOURrEU2LBMRq8C";
+    playerContainer;
 
 
     function init(){
       console.log("video is auch");
 
-
-
-
-
-
-
-
-    /*  playerContainer = document.getElementById('player-container');
-      playerContainer.innerHTML = '<iframe src="' + videoUrl + '"></iframe>'
-      console.log(playerContainer.innerHTML);*/
     }
-
 
 
 
