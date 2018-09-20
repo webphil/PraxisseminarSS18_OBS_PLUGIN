a10b1ad3199781adf39fe5acac1e1718e1690404
Julia Grötsch <julia@groetsch.org>
2018-09-20 22:27:11 +0200

    updates in "loadExistingTests"

--- Code/Usability Testing Platform/js/loadExistingTest.js
+++ Code/Usability Testing Platform/js/loadExistingTest.js
@@ -9,7 +9,6 @@ App.loadExistingTest = function () {
 
 
   function init(){
-    handleExistingTestUserInteractions();
     loadExistingTestsFromDatabase();
   }
 
@@ -18,47 +17,49 @@ App.loadExistingTest = function () {
 
   function handleExistingTestUserInteractions(){
     $('.table > tbody > tr').click(function() {
-    selectedTest = $(this).find('th').text();
-    displaySelectedTest(selectedTest);
+    var selectedTest = $(this).find('td')[0].innerHTML;
+    displayTest(selectedTest);
     });
+
   }
 
 
 
-    function displaySelectedTest(tests){
-    }
-
-
-
-
-
-
     function loadExistingTestsFromDatabase(){
+      var table = document.getElementById("tableTests");
+      table.innerHTML = "";
+      var tableBody = document.createElement('tbody');
+      table.append(tableBody);
       $.ajax({
         type: "POST",
         url: '../php/loadTests.php',
         dataType: "json",
         success: function(data) {
-          processTests(data);
+          fillTable(data);
+          handleExistingTestUserInteractions();
         }
       });
     }
 
-    function processTests(data){
-      console.log("Done");
-      console.log(""+data[1]);
+    function fillTable(data){
+      var i;
+      for(i = 0; i < data.length; i ++){
+        var tests = $.parseJSON(data[i]);
+        var tableRef = document.getElementById('tableTests').getElementsByTagName('tbody')[0];
+        var newRow   = tableRef.insertRow(tableRef.rows.length);
+        var newCellNumber = newRow.insertCell(0)
+        var newCellTitle  = newRow.insertCell(1);
+        var newCellDate = newRow.insertCell(2);
+        newCellNumber.innerHTML = ""+ (i+1);
+        newCellTitle.innerHTML = tests.title;
+        newCellDate.innerHTML = tests.date;
+      }
     }
 
 
-
-
-
-
-
-
-
-
-
+    function displayTest(selectedTest){
+      console.log(selectedTest);
+    }
 
 
 
